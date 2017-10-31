6040f1ab85ba7c02949b61b4922e0e1643f296dc
WIN_NT\x15g010 <x15g010@chiba-fjb.ac.jp>
2017-10-31 14:27:02 +0900

    バックボタン

--- app/src/main/java/jp/ac/chiba_fjb/f/home/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/f/home/MainActivity.java
@@ -1,11 +1,14 @@
 package jp.ac.chiba_fjb.f.home;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -49,5 +52,27 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            // なんらかの処理
+            AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+            alertDlg.setTitle("true");
+            alertDlg.setPositiveButton(
+                    "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // OK ボタンクリック処理
+                        }
+                    });
+
+            // 表示
+            alertDlg.create().show();
+            return true;
+        }
+        System.out.print("false");
+        return false;
     }
 }
